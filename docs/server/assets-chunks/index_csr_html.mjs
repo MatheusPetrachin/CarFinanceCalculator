fc77c6a3c1e6974fef584fecee5e3246e489901f
Matheus Roque Petrachin <mrp.petrachin@gmail.com>
2025-06-30 21:15:29 -0300

    dev

---
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,17 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+
+<head>
+  <meta charset="utf-8">
+  <title>CarFinanceCalculator</title>
+  <base href="C:/Program Files/Git/CarFinanceCalculator/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles-5INURTSO.css"></head>
+
+<body ngcm="">
+  <app-root></app-root>
+<script src="main-J5IQE3HV.js" type="module"></script></body>
+
+</html>
+`;
